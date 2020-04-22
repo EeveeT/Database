bdfcda2d1d2130908eb4455415f18fe68d95fb4a
Evy Tang <yvette_tang@outlook.com>
2020-04-22 17:11:12 +0100

    Completed tokeniser (I think)

--- src/Parsers/Tokeniser.java
+++ src/Parsers/Tokeniser.java
@@ -1,15 +1,47 @@
 package Parsers;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 import static Parsers.TokenType.*;
 import static java.lang.Character.isDigit;
+import static java.lang.Character.isLetterOrDigit;
+import static java.lang.Character.isLetter;
 
 
 
 public class Tokeniser
 {
     // A specific type or parser
+
+    private static final Map<String, TokenType> keywords;
+
+    static{
+        keywords = new HashMap<>();
+        keywords.put("SELECT", SELECT);
+        keywords.put("USE", USE);
+        keywords.put("CREATE", CREATE);
+        keywords.put("DROP", DROP);
+        keywords.put("ALTER", ALTER);
+        keywords.put("INSERT", INSERT);
+        keywords.put("UPDATE", UPDATE);
+        keywords.put("DELETE", DELETE);
+        keywords.put("JOIN", JOIN);
+        keywords.put("DATABASE", DATABASE);
+        keywords.put("TABLE", TABLE);
+        keywords.put("INTO", INTO);
+        keywords.put("VALUES", VALUES);
+        keywords.put("FROM", FROM);
+        keywords.put("SET", SET);
+        keywords.put("WHERE", WHERE);
+        keywords.put("ON", ON);
+        keywords.put("TRUE", TRUE);
+        keywords.put("FALSE", FALSE);
+        keywords.put("AND ", AND);
+        keywords.put("OR", OR);
+    }
 
     private final String source;
     private final List<Token> tokens;
@@ -66,6 +98,12 @@ public class Tokeniser
                 if(matches('=')) { addToken(NOT_EQUAL_TO); }
                 break;
 
+            case '\'':
+                scanString();
+                break;
+
+
+
             // Ignores whitespaces
             case ' ':
             case '\r':
@@ -74,13 +112,18 @@ public class Tokeniser
                 break;
 
 
+
             default:
                 if(isDigit(ch)){
                     scanNumber();
                 }
+                else if(isLetter(ch)){
+                    scanWord();
+                }
                 else {
                     throw new UnexpectedCharacterException();
                 }
+
         }
     }
 
@@ -132,6 +175,30 @@ public class Tokeniser
         }
         addToken(NUMBER);
     }
+
+    private void scanString() throws UnexpectedCharacterException {
+
+        while(isLetterOrDigit(lookAhead())){
+            nextChar();
+        }
+        if(matches('\'')){
+            addToken(STRING);
+        }
+        else{
+            throw new UnexpectedCharacterException();
+        }
+    }
+
+    private void scanWord(){
+
+        while(isLetter(lookAhead())){
+            nextChar();
+        }
+        String text = source.substring(start, current);
+        addToken(keywords.getOrDefault(text, VARIABLE));
+
+    }
+
 
 }
 
